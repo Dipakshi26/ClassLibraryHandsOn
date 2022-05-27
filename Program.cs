edee5f844c70bfe5aea7e6c363799802427379ae
Dipakshi Saxena <75835612+Dipakshi26@users.noreply.github.com>
2022-05-27 14:45:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EbillClass;
+
+namespace EbillPrroject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EBill eBill = new EBill();
+            eBill.EnterDetails();
+           
+            Customers customer = new Customers();
+            customer.ShowDetails(11);
+
+            customer.ShowEBill(11);
+
+            customer.ShowEbBillReport();
+
+
+        }
+    }
+}
